c16cb6a0a09f787357424354b48ce525927b2751
Muhammad Salik Salam <salik.salam@cherre.com>
2026-06-04 15:57:26 -0500

    fix md lint

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -10,4 +10,15 @@ return {
       },
     },
   },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = function(_, opts)
+      local config = vim.fn.stdpath("config") .. "/.markdownlint.yaml"
+      opts.linters = opts.linters or {}
+      opts.linters["markdownlint-cli2"] = vim.tbl_deep_extend("force", opts.linters["markdownlint-cli2"] or {}, {
+        args = { "--config", config, "-" },
+      })
+    end,
+  },
 }
